9f2600599da40db6975dc446065b4d440d1f17bb
Alex-Folz <alexander.folz@protonmail.com>
2026-07-20 08:36:05 +0200

    Initial nvim config

---
+++ init.lua
@@ -0,0 +1,80 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "nvim-treesitter/nvim-treesitter",
+  "neovim/nvim-lspconfig",
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+
+  "mason-org/mason-lspconfig.nvim",
+
+  {
+    "folke/noice.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      view = { width = 35 },
+      renderer = { group_empty = true },
+      filters = { dotfiles = false },
+    },
+  },
+
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+      completion = { documentation = { auto_show = true } },
+    },
+  },
+})
+
+-- LSP setup
+vim.lsp.enable({ 'pyright', 'ts_ls', 'intelephense', 'twiggy_language_server' })
+
+-- Keymaps
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { desc = "Toggle File Tree" })
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live Grep" })
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to Definition" })
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Hover Documentation" })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Actions" })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show Error" })
